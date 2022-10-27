5843f63b8d0809bcae0f5a22ef9e9ed0f81b1346
Abhinav Kumar <112795912+abhinav-1308@users.noreply.github.com>
2022-10-27 21:19:22 +0530

    DSA 26/10/22

---
+++ DSA/LAB8-26-10-22/4.c
@@ -0,0 +1,19 @@
+/*WA menu diven program to implement double linked list and do the following operation
+insert at begging insertiond at end
+delete at begging delete at end
+display or traverse from first to last and last to first*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+
+    int info;
+    
+};
+struct node* sart = NULL;
+void tranverse(){
+    if start == NULL{
+
+         
+    }
